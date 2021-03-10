62b504a295dacc31eb5d6306761fbfd5b4f54463
carlirows <carlirows@gmail.com>
2021-03-10 18:12:13 -0500

    conection to DB, bootcamp model created, CRUD implemented

---
+++ controlers/bootcamps.js
@@ -0,0 +1,79 @@
+const Bootcamp = require('../models/bootcamp')
+
+//@desc Get all bootcamps
+//@route GET /api/v1/bootcamps
+//@access Public
+exports.getBootcamps = async (req,res) => {
+    try {
+        const bootcamps = await Bootcamp.find()
+        res.send({ success: true, count: bootcamps.length, data: bootcamps })    
+    } catch (err) {
+        res.status(400).send({
+            success: false, 
+        })
+    }
+}
+
+//@desc Get bootcamp by id
+//@route GET /api/v1/bootcamps/:id
+//@access Public
+exports.getBootcamp = async(req,res) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id)
+        if(!bootcamp){
+            res.status(404).send({ success: false, error: 'no bootcamp matches the Id provided' })
+        }
+        res.send({ success: true, data: bootcamp })
+    } catch (err) {
+        res.status(404).send({ success: false })    
+    }    
+}
+
+//@desc Post bootcamp by id
+//@route POST /api/v1/bootcamps/
+//@access private
+exports.createBootcamp = async(req,res) => {
+   try {
+        const bootcamp = await Bootcamp.create(req.body)
+        res.send({ success: true, count: bootcamp.length, data:bootcamp  })    
+    } catch (err) {
+       res.status(400).send({ success: false })       
+   }
+    
+}
+
+//@desc Put bootcamp by id
+//@route PUT /api/v1/bootcamps/:id
+//@access Private
+exports.updateBootcamp = async(req,res) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        if(!bootcamp){
+            res.status(404).send({ success: false, error: 'no bootcamp matches the Id provided' })
+        }
+    
+        res.send({ success: true, data: bootcamp })
+    } catch (err) {
+        res.status(404).send({ success: false })
+    }
+    
+}
+
+//@desc Delete bootcamp by id
+//@route DELETE /api/v1/bootcamps/:id
+//@access Private
+exports.deleteBootcamp = async(req,res) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+        if(!bootcamp){
+            res.status(404).send({ success: false, error: 'no bootcamp matches the Id provided' })
+        }
+    
+        res.send({ success: true, data: bootcamp })
+    } catch (err) {
+        res.status(404).send({ success: false })
+    }
+}
--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const colors = require('colors')
+const connectDB = require('./config/db')
+
+//route files
+const bootcamps = require('./routes/bootcamps')
 
 //load dotenv variables
-dotenv.config({path: './config/config.env' })
+dotenv.config({ path: './config/config.env' })
 
+//connect to database
+connectDB()
 
+// creating express instance
 const app = express()
+
+//bodyparser
+app.use(express.json())
+
+//dev loggin middleware
+app.use(morgan('dev'))
+
+//mount router
+app.use('/api/v1/bootcamps', bootcamps)
+
+
 
 const PORT = process.env.PORT
 
-app.listen(PORT, ()=>{
-    console.log('server up and running on port ', PORT)
+const server = app.listen(PORT, ()=>{
+    console.log(`server up and running on port ${PORT}`.yellow.bold)
+})
+
+//handle unhandled promise rejetion
+process.on('unhandledRejection', (err, promise)=>{
+    console.log(`Error: ${err.message}`.red)
+    //Close server & exit proccess
+    server.close(()=> process.exit(1))
 })
