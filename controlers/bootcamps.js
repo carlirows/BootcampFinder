6637c54febd2944dd24b213c9ef4a6b0f783389d
carlirows <carlirows@gmail.com>
2021-03-11 20:07:09 -0500

    select, sort and pagination implemented

--- controlers/bootcamps.js
+++ controlers/bootcamps.js
@@ -8,9 +8,75 @@ const { geocode } = require('../utils/geocoder')
 //@route GET /api/v1/bootcamps
 //@access Public
 exports.getBootcamps = asyncHandler(async (req,res, next) => {
-  
-        const bootcamps = await Bootcamp.find()
-        res.send({ success: true, count: bootcamps.length, data: bootcamps })    
+        let query;
+        //Create a copy of rq.query
+        const reqQuery = { ...req.query }
+
+        //specifying fields to exclude from the query
+        const removeFields = ['select', 'sort', 'page', 'limit']
+
+        //looping over remove fields, and deleting fields from copy of req.query
+        removeFields.forEach(field => delete reqQuery[field])
+
+        console.log(reqQuery)
+        
+//setUp to use mongoOperators in the query
+        //1.creating query string to manipulate
+        let queryStr = JSON.stringify(reqQuery)
+
+        //creating mongo filtering operators $gt $gte $lt
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
+
+        //finding resource that matches 
+        query = Bootcamp.find(JSON.parse(queryStr))
+
+//select
+        //once ive found the resource, i can choose to show some or all of its properties
+        //select fields /?select=name,description
+        if(req.query.select) {               
+                const fields = req.query.select.split(',').join(' ');
+                console.log(fields)
+                query = query.select(fields)
+        }
+
+//Sort
+        if(req.query.sort) {
+                const sortBy = req.query.sort.split(',').join(' ')
+                query = query.sort(sortBy) 
+        } else {
+                query = query.sort('-createdAt')
+        }
+
+//pagination
+        const page = parseInt(req.query.page, 10) || 1
+        const limit = parseInt(req.query.limit, 10) || 25
+        const startIndex = (page -1) * limit
+        const endIndex = page * limit
+        const total = await Bootcamp.countDocuments()
+
+        query = query.skip(startIndex).limit(limit)
+
+
+        //executing the query
+        const bootcamps = await query
+
+        //pagination result
+        const pagination = {}
+        if(endIndex < total){
+                pagination.next = {
+                        page: page + 1,
+                        limit
+                }
+        }
+        if(startIndex > 0) {
+                pagination.prev = {
+                        page: page -1,
+                        limit
+                }
+        }
+
+        
+        res.send({ success: true, count: bootcamps.length, pagination, data: bootcamps })    
  
 })
 
